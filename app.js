2c154473f8c10d18a62ae22cd3e881d338ab1639
Anuv Gupta <anuv.ca@gmail.com>
2016-10-02 18:56:17 -0700

    remove +

--- app.js
+++ app.js
@@ -129,14 +129,14 @@ $(document).ready(function () {
                 $(window).resize();
                 var index = window.location.href.indexOf('#');
                 if (index != -1) {
- +                    var id = window.location.hash;
- +                    if (id == '#source' || id == '#source-code')
-                          sitarhero.child('tabs/github').on('click', { noscroll: true });
- +                    if (document.getElementById(id.substring(1)) != null) {
-                          $(document.body).animate({
- +                            scrollTop: $(id).offset().top + 'px'
-                          }, /* 700 */ 0);
-                      }
+                    var id = window.location.hash;
+                    if (id == '#source' || id == '#source-code')
+                        sitarhero.child('tabs/github').on('click', { noscroll: true });
+                    if (document.getElementById(id.substring(1)) != null) {
+                        $(document.body).animate({
+                            scrollTop: $(id).offset().top + 'px'
+                        }, /* 700 */ 0);
+                    }
                   }
             }
         });
